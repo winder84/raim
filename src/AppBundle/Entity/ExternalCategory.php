887e3df751302461a91c0fd089f214727e9e6a74
rustam <rustam@nodasoft.com>
2016-12-29 14:30:31 +0300

    nullable

--- src/AppBundle/Entity/ExternalCategory.php
+++ src/AppBundle/Entity/ExternalCategory.php
@@ -17,7 +17,7 @@ class ExternalCategory
      *
      * @ORM\Column(name="id", type="integer")
      * @ORM\Id
-     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\GeneratedValue(strategy="AUTO", nullable=true)
      */
     private $id;
 
